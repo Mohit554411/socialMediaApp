0c695bb0a253bd910ed7c4e4e9c8ef628462d977
mohit.kumar <mohit.kumar@transporeon.com>
2024-08-03 22:18:01 +0530

    Second commit

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 import express from 'express';
+import expressEjsLayouts from 'express-ejs-layouts';
+import path from 'path';
+
+const app = express();
+
+app.set('view engine','ejs');
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(expressEjsLayouts);
+app.use(express.static('public'));
+app.use(express.static('src/views'));
+app.set('views',path.join(path.resolve(), 'src','views'));
+
+app.get('/', (req, res) => {
+    res.render('layout');
+});
+
+app.listen(3000,()=>{
+    console.log('Server is running on port 3000');
+})
